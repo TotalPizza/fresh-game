3ccf462a11b6ff374b99e89e350d0544779db703
Fresh Pizza <fpizza@protonmail.com>
2023-03-10 22:24:11 +0100

    started seeds and fields table

--- src/components/farm_menu.tsx
+++ src/components/farm_menu.tsx
@@ -1,28 +1,81 @@
 import Image from 'next/image'
 import styles from '@/styles/components/menu.module.css'
 import XButton from './x_button'
+import {tokens} from '@/hooks/token_list'	
 
 export default function FarmMenu(props: {show: boolean, toggle_farm_menu: () => void}) {
+    
     return (
         <div className={styles.menu} hidden={!props.show}>
             <Image className={styles.menu_background} alt={"menu_background"} src='/images/menu_background.png' width={1000} height={750}/>
             <XButton margin_left={890} margin_top={10} close_action={props.toggle_farm_menu}/>
-            <table className={styles.asset_table}>
-                <thead className={styles.asset_table_head}>Seeds</thead>
+            <div className={styles.seeds_heading}>Seeds</div>
+            <table className={styles.seeds_table}>
                 <tbody>
-                    <tr>
-                        <td>
-                            <Image className={styles.asset_icon} alt={"eth"} src='/seeds/eth.png' width={80} height={80}/>
-                        </td>
-                    </tr>
                     <br/>
-                    <tr>
-                        <td>
-                            <Image className={styles.asset_icon} alt={"eth"} src='/seeds/eth.png' width={80} height={80}/>
-                        </td>
-                    </tr>
+                    <SeedsList/>
                 </tbody>
             </table>
+            <table className={styles.fields_table}>
+                <tbody>
+                    <br/>
+                    <FieldsList/>
+                </tbody>
+            </table>
+            <div className={styles.fields_heading}>Fields</div>
         </div>
+    )
+}
+
+function SeedsList(){
+    let seeds: JSX.Element[] = [];
+    tokens.forEach(token => {
+        seeds.push(
+            <tr className={styles.seeds_row}>
+                <td className={styles.seeds_col}>
+                    <Image className={styles.asset_icon} alt={token.name} src={token.src} width={80} height={80}/>
+                </td>
+                <td className={styles.seeds_col}>
+                    {token.name}
+                </td>
+                <td className={styles.seeds_col}>
+                    {'0.00'}
+                </td>
+            </tr>
+        ) 
+    });
+    return(
+        <>
+            {seeds.map((data) => {
+                return(
+                    data
+                )
+            })}
+        </>
+    )
+}
+
+function FieldsList(){
+    let fields: JSX.Element[] = [];
+    tokens.forEach(token => {
+        fields.push(
+            <tr className={styles.fields_row}>
+                <td className={styles.fields_col}>
+                    <Image className={styles.asset_icon} alt={token.name} src={token.src} width={80} height={80}/>
+                </td>
+                <td className={styles.fields_col}>
+                    {"Nostra"}
+                </td>
+            </tr>
+        ) 
+    });
+    return(
+        <>
+            {fields.map((data) => {
+                return(
+                    data
+                )
+            })}
+        </>
     )
 }
