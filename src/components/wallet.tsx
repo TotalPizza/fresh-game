2371355aebafd13ebaf32694c2b4d9ede72a81d6
Fresh Pizza <fpizza@protonmail.com>
2023-03-10 12:54:15 +0100

    Add starknet libs

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -7,8 +7,8 @@ export default function WalletButton() {
             <Image className={styles.frame} alt={"wallet_frame"} src='/wallet_frame.png' width={150} height={150}/>
             <Image className={styles.glas} alt={"wallet_glas"} src='/wallet_glas.png' width={145} height={145}/>
             <Image className={styles.edge} alt={"wallet_edge"} src='/wallet_edge.png' width={145} height={145}/>
-            <Image className={styles.logo} alt={"wallet_logo"} src='/logos/argent_icon.png' width={145} height={145}/>
-            <Image className={styles.logo_hover} alt={"wallet_logo_hover"} src='/logos/argent_icon_hover.png' width={145} height={145}/>
+            <Image className={styles.logo} alt={"wallet_logo"} src='/logos/braavos_icon.png' width={145} height={145}/>
+            <Image className={styles.logo_hover} alt={"wallet_logo_hover"} src='/logos/braavos_icon_hover.png' width={145} height={145}/>
         </div>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import NextHead from 'next/head'
+import { Analytics } from '@vercel/analytics/react';
+import { RpcProvider, RpcProviderOptions } from 'starknet'
 import '@/styles/globals.css'
+import { StarknetConfig, InjectedConnector } from '@starknet-react/core'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const infuraURL: RpcProviderOptions = {nodeUrl: "https://starknet-mainnet.infura.io/v3/a83025cd6f6246ae86cfe05ba6fe630e"}
+  const provider = new RpcProvider(infuraURL)
+  const connectors = [
+    new InjectedConnector({ options: { id: 'braavos' }}),
+    new InjectedConnector({ options: { id: 'argentX' }}),
+  ]
+
+  return (
+    <StarknetConfig connectors={connectors} defaultProvider={provider}>
+      <NextHead>
+        <title>Commercium</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta name="description" content="The Commercium, providing on-chain DEX aggregation on StarkNet." />
+        <link rel="icon" href='./images/favicon.png'/>
+      </NextHead>
+      <Component {...pageProps} />
+      <Analytics/>
+    </StarknetConfig>
+  )
 }
