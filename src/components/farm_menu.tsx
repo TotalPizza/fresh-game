bcc9e022bb0d66301d1ca349306cdc8a001f0cd5
Fresh Pizza <fpizza@protonmail.com>
2023-03-10 20:04:55 +0100

    small commit

--- src/components/farm_menu.tsx
+++ src/components/farm_menu.tsx
@@ -8,10 +8,17 @@ export default function FarmMenu(props: {show: boolean, toggle_farm_menu: () => 
             <Image className={styles.menu_background} alt={"menu_background"} src='/images/menu_background.png' width={1000} height={750}/>
             <XButton margin_left={890} margin_top={10} close_action={props.toggle_farm_menu}/>
             <table className={styles.asset_table}>
+                <thead className={styles.asset_table_head}>Seeds</thead>
                 <tbody>
                     <tr>
                         <td>
-                            <Image className={styles.asset_icon} alt={"eth"} src='/token_icons/eth.png' width={50} height={50}/>
+                            <Image className={styles.asset_icon} alt={"eth"} src='/seeds/eth.png' width={80} height={80}/>
+                        </td>
+                    </tr>
+                    <br/>
+                    <tr>
+                        <td>
+                            <Image className={styles.asset_icon} alt={"eth"} src='/seeds/eth.png' width={80} height={80}/>
                         </td>
                     </tr>
                 </tbody>
