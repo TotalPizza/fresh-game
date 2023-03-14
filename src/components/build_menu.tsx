743964cf50fecf7fb8f4873ff39642c9bc4556ce
Fresh Pizza <fpizza@protonmail.com>
2023-03-14 09:29:54 +0100

    Set a bunch of image priorities

--- src/components/build_menu.tsx
+++ src/components/build_menu.tsx
@@ -6,11 +6,11 @@ import { BuildItem, BuildType } from '@/components/cursor_item'
 export default function BuildMenu(props: {building_status: boolean[],show: boolean, toggle_build_menu: () => void, placing_building: (item: BuildItem) => void}) {    
     return (
         <div className={styles.menu} hidden={!props.show}>
-            <Image className={styles.menu_background} alt={"menu_background"} src='/images/menu_background.png' width={1000} height={750}/>
+            <Image className={styles.menu_background} priority={true} alt={"menu_background"} src='/images/menu_background.png' width={1000} height={750}/>
             <XButton margin_left={750} margin_top={-700} close_action={props.toggle_build_menu}/>
             <div className={styles.building_heading}>Buildings</div>
-            <Image className={styles.building_icon_frame} alt={"mill_icon_frame"} src='/images/frame.png' width={100} height={100}/>
-            <Image className={styles.building_icon_frame_hover} onClick={()=>{
+            <Image className={styles.building_icon_frame} priority={true} alt={"mill_icon_frame"} src='/images/frame.png' width={100} height={100}/>
+            <Image className={styles.building_icon_frame_hover} priority={true} onClick={()=>{
                     // Do Nothing if Mill is already placed
                     if(props.building_status[0]){
                         return;
@@ -18,8 +18,8 @@ export default function BuildMenu(props: {building_status: boolean[],show: boole
                         props.placing_building({building: BuildType.Mill, src:'/images/mill_white.png', width: 300, height: 300})
                     };
                 }}  alt={"mill_icon_frame_hover"} src='/images/frame_hovered.png' width={109} height={109}/>
-            <Image className={styles.building_icon} alt={"mill_icon"} src='/images/nostra_mill_icon.png' width={100} height={100}/>
-            <Image className={styles.building_icon_disabled} style={{opacity: +props.building_status[0]}} alt={"mill_icon_disabled"} src='/images/nostra_mill_icon_disabled.png' width={100} height={100}/>
+            <Image className={styles.building_icon} priority={true} alt={"mill_icon"} src='/images/nostra_mill_icon.png' width={100} height={100}/>
+            <Image className={styles.building_icon_disabled} priority={true} style={{opacity: +props.building_status[0]}} alt={"mill_icon_disabled"} src='/images/nostra_mill_icon_disabled.png' width={100} height={100}/>
         </div>
     )
 }
--- src/components/farm_menu.tsx
+++ src/components/farm_menu.tsx
@@ -11,7 +11,7 @@ export default function FarmMenu(props: {show: boolean, toggle_farm_menu: () => 
     
     return (
         <div className={styles.menu} hidden={!props.show}>
-            <Image className={styles.menu_background} alt={"menu_background"} src='/images/menu_background.png' width={1000} height={750}/>
+            <Image className={styles.menu_background} priority={true} alt={"menu_background"} src='/images/menu_background.png' width={1000} height={750}/>
             <XButton margin_left={750} margin_top={-700} close_action={props.toggle_farm_menu}/>
             <div className={styles.seeds_heading}>Seeds</div>
             <table className={styles.seeds_table}>
@@ -45,7 +45,7 @@ function SeedsList(props:{selected_token: Token, set_selected_token: (token: Tok
             // tr has different color when hovered
             <tr className={styles.seeds_row} style={{backgroundColor: seed_row_color}} onClick={()=>props.set_selected_token(token.token)}>
                 <td className={styles.seeds_col}>
-                    <Image className={styles.asset_icon} alt={token.name} src={token.src} width={80} height={80}/>
+                    <Image className={styles.asset_icon} priority={true} alt={token.name} src={token.src} width={80} height={80}/>
                 </td>
                 <td className={styles.seeds_col}>
                     {token.name}
@@ -88,7 +88,7 @@ function FieldsList(props:{token: Token, placing_field: (protocol: Protocol, amo
             fields.push(
                 <tr className={styles.fields_row} onClick={() => props.placing_field(protocol,token_amount,props.token)}>
                     <td className={styles.fields_col}>
-                        <Image className={styles.asset_icon} alt={protocol_name[protocol]} src={protocol_farm_icon[protocol]} width={80} height={80}/>
+                        <Image className={styles.asset_icon} priority={true} alt={protocol_name[protocol]} src={protocol_farm_icon[protocol]} width={80} height={80}/>
                     </td>
                     <td className={styles.fields_col}>
                         {token_amount}
