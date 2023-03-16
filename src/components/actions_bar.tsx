9f4994a2411ca58f4bcf5b79d4dcbc4c9ce0ce61
Fresh Pizza <fpizza@protonmail.com>
2023-03-16 13:43:11 +0100

    More explenations

--- src/components/actions_bar.tsx
+++ src/components/actions_bar.tsx
@@ -6,6 +6,7 @@ import { Instruction, Action } from '@/utils/interfaces'
 import { protocol_lend_action } from '@/hooks/action_list'
 import { token_name } from '@/hooks/token_list'
 import { InstructionToTX } from '@/hooks/instruction_to_tx'
+import { useState } from 'react'
 
 interface ActionDisplay{
     action_info: string,
@@ -13,6 +14,7 @@ interface ActionDisplay{
 }
 
 export function ActionsBar(props:{account_address: string|undefined, instructions: Instruction[], clear_instructions: () => void, pop_instruction: () => void}){
+    const [showUndoText, setShowUndoText] = useState(0);
 
     let actions: ActionDisplay[] = [];
     props.instructions.forEach((instruction) => {
@@ -60,8 +62,9 @@ export function ActionsBar(props:{account_address: string|undefined, instruction
             </table>
             <Image className={styles.actions_bar_banner} alt={"actions_bar"} src={'/images/actions_bar_banner.png'} width={238} height={900}/>   
             <ExecuteTransactions calls={calls} clear_instructions={props.clear_instructions}/>
-            <Image className={styles.undo} alt={"undo_button"} src={'/images/undo.png'} width={50} height={75}/>   
-            <Image className={styles.undo_hover} onClick={props.pop_instruction} alt={"undo_button_hover"} src={'/images/undo_hover.png'} width={50} height={75}/>   
+            <Image className={styles.undo} alt={"undo_button"} src={'/images/undo.png'} width={50} height={75}/>  
+            <div className={styles.undo_text} style={{opacity: showUndoText}}>Undo Action</div> 
+            <Image className={styles.undo_hover} onMouseEnter={() => setShowUndoText(1)} onMouseLeave={() => setShowUndoText(0)} onClick={props.pop_instruction} alt={"undo_button_hover"} src={'/images/undo_hover.png'} width={50} height={75}/>   
             <Image className={styles.undo_frame} alt={"undo_button_frame"} src={'/images/undo_frame.png'} width={50} height={75}/>    
         </>
     )
